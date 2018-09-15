d33aec39bff1ac400986a32b96b08f93c9f0c44f
Nathan Savas <nathansavas@gmail.com>
2018-09-14 21:47:56 -0700

    link server data to components
    each component within the main app component is now pulling its property data from the server. Not actual server data yet but getting there lol

--- src/App.js
+++ src/App.js
@@ -1,37 +1,130 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch}
-from 'react-router-dom';
 import './App.css';
 
-import Login from './Components/Login';
+const headerStyle = {
+  "font-size":"50px"
+}
+
+const fakeServerData = {
+  user: {
+    name: 'Bob',
+    genreCount: '13',
+    artistCount: '67',
+    genres: [
+      {
+        name: 'Rock & Roll',
+        artists: ['Led Zeppelin', 'Pink FLoyd', 'Deep Purple']
+      },
+      {
+        name: 'Hip Hop',
+        artists: ['Eazy-E', 'Tupac', 'Notorious B.I.G']
+      },
+      {
+        name: 'EDM',
+        artists: ['Skrillex', 'Flux Pavilion', 'Diplo', 'Tsuruda']
+      }
+    ]
+  }
+}
+
+class GenreCounter extends Component {
+  render() {
+    return (
+      <div>
+      <h2>
+        You listen to {this.props.genreCount} different genres.
+      </h2>
+      </div>
+    )
+  }
+}
+
+class ArtistCounter extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>
+            and {this.props.artists} different artists!
+          </h2>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Genres extends Component {
+  render() {
+
+    const allGenres = this.props.genres.reduce((genres, eachGenre) => {
+      return genres.concat(eachGenre.name)
+    }, []) 
+
+    return (
+      <div>
+        <h2>
+          Here are the genres: {allGenres}
+        </h2>
+      </div>
+    )
+  }
+}
+
+class Artists extends Component {
+  render() {
+
+    const allArtists = this.props.artists.reduce((genres, eachGenre) => {
+      return genres.concat(eachGenre.artists)
+    }, []) 
+
+    return (
+      <div>
+        <h2>
+          Here are the artists: {allArtists}
+        </h2>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-        fakeServerData: {
-          genres: {},
-          artists: {}
-        }
-      }
-    }
+      serverData: {}
+    };
+  };
 
-  ComponentDidMount() {
-    this.setState = {
-      genres: {},
-      artists: {}
-    }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({serverData:fakeServerData});
+    }, 1000);
   }
 
   render () {
     return (
-      <Router>
-        <div className="App">
-          <Login/>
+      <div className="App">
+      {this.state.serverData.user ?
+        <div>
+          <h1 style={{...headerStyle}}>
+            Hi {this.state.serverData.user.name}! Here is your listening data:
+          </h1>
+          <h2>
+            <GenreCounter genreCount={this.state.serverData.user &&
+                                  this.state.serverData.user.genreCount}/>
+            <ArtistCounter artists={this.state.serverData.user &&
+                                    this.state.serverData.user.artistCount}/>
+            <Genres genres={this.state.serverData.user &&
+                            this.state.serverData.user.genres}/>
+            <Artists artists={this.state.serverData.user &&
+                              this.state.serverData.user.genres}/>
+
+          </h2>
+        </div> : <h1 style={{...headerStyle}}>Loading...</h1>
+      }
       </div>
-      </Router>
     );
-  }
+  };
 };
 
 export default App;
